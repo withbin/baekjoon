843503a7de9625b79ffe5888557dbc062f56aaf4
Jaebin Lee <woqls2323@g.skku.edu>
2021-12-14 13:25:42 +0900

    new add

---
+++ 에라토스테네스의 체.py
@@ -0,0 +1,9 @@
+a,b=map(int,input().split())
+list2=[True]*(b+1)
+for i in range(2,int(b**0.5)+1):
+    if list2[i]==True:
+        for j in range(i+i,b+1,i):
+            list2[j]=False
+for i in range(a,b+1):
+    if list2[i]==True:
+        print(i)
